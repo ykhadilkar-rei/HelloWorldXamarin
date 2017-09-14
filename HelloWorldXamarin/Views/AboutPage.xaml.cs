71b7a3277f03cc4aff15a64514d232a96531a2c7
ykhadilkar-rei <ykhadilkar@reisys.com>
2017-09-14 11:22:31 -0400

    initial commit

---
+++ HelloWorldXamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloWorldXamarin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
